db1e0bf6a1b7f9e25f909e00761ff55b9f7cfd31
Max <nguyen.ba.manh@moneyforward.vn>
2020-06-27 15:56:16 +0700

    Add JS for Cart

---
+++ js/giohang.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+  calcTotal();
+});
+
+$('.btn-remove').click(function(e) {
+  var td = e.target.parentElement;
+  var tr = td.parentElement;
+  tr.remove();
+  calcTotal();
+})
+
+
+function calcTotal() {
+  var total = getSum($('.price-value'));
+  $('#total-amount').text(total);
+  $('#total-discount').text(total - 20000);
+}
+
+function getSum(items) {
+  var total = 0;
+  for (i = 0; i < items.length; i++) {
+    var price = parseInt(items[i].value);
+    total += price;
+  }
+  return total;
+}
